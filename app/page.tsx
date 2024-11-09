d2c6be54582f5b08f0baed7d810eea19ebef9c2e
Darshh7 <86665101+Darshh7@users.noreply.github.com>
2024-11-09 17:30:43 +0530

    Made some changes related to setup and added icons

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className={"center"}>
+      <h1 className="h1">
+        PocketCloud - The only storage solution you need, right in your pocket
+      </h1>
+    </div>
+  );
+}
